a9ad2606366313b045ab7c255f55996e603a3c2f
Mohammed Mukarram <53473599+ryuoga12@users.noreply.github.com>
2020-06-11 20:08:47 +0530

    Create segoddeven.java

---
+++ segoddeven.java
@@ -0,0 +1,33 @@
+import java.io.*; 
+public class SegregateOddEven {
+	static void segregateEvenOdd(int arr[]) 
+	    { 
+	        int left = 0, right = arr.length - 1; 
+	        while (left < right) 
+	        { 
+	            while (arr[left]%2 == 0 && left < right) 
+	                left++; 
+	            while (arr[right]%2 == 1 && left < right) 
+	                right--; 
+	  
+	            if (left < right) 
+	            { 
+	                /* Swap arr[left] and arr[right]*/
+	                int temp = arr[left]; 
+	                arr[left] = arr[right]; 
+	                arr[right] = temp; 
+	                left++; 
+	                right--; 
+	            } 
+	        } 
+	    } 
+	    public static void main (String[] args) 
+	    { 
+	        int arr[] = {12, 34, 45, 9, 8, 90, 3}; 
+	        segregateEvenOdd(arr); 
+	        System.out.print("Array after segregation "); 
+	        for (int i = 0; i < arr.length; i++) 
+	            System.out.print(arr[i]+" "); 
+	    } 
+	
+}
